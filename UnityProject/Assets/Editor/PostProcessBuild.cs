1f5c8755bb38cd7d9e8e435e0706ba8ace571a08
Antony Nasce <nasce@mac.com>
2024-11-20 09:36:17 +0000

    Add UnityFramework Build Step to allow Apple Core Plugins to be embedded.

--- UnityProject/Assets/Editor/PostProcessBuild.cs
+++ UnityProject/Assets/Editor/PostProcessBuild.cs
@@ -1,5 +1,6 @@
 using UnityEditor;
 using UnityEditor.iOS.Xcode;
+using System.Collections.Generic;
 
 /* We could implement UnityEditor.Build.IPostprocessBuildWithReport interface
    but documentation is unclear if this is preferred over the below callback attribute.
@@ -39,6 +40,59 @@ public class PostProcessBuild
             FileUtil.ReplaceFile("Assets/Plugins/iOS/UnityFramework.modulemap", modulemapAbsolutePath);
             project.AddFile(modulemapAbsolutePath, modulemapRelativePath);
             project.AddBuildProperty(unityFrameworkTargetGuid, "MODULEMAP_FILE", modulemapRelativePath);
+            string embedApplePluginScript = @"#  Apple Unity Plug-in Sign & Embed libraries shell script
+#  Copyright © 2024 Apple, Inc. All rights reserved.
+#  This script is added to the generated Xcode project by the Apple.Core plug-in.
+#  Please see AppleBuild.cs in the Apple.Core plug-in for more information.
+dstFrameworkFolder=""$BUILT_PRODUCTS_DIR/$FRAMEWORKS_FOLDER_PATH""
+dstBundleFolder=""$BUILT_PRODUCTS_DIR/$PLUGINS_FOLDER_PATH""
+APPLE_PLUGIN_LIBRARY_ROOT=""$PROJECT_DIR/ApplePluginLibraries""
+if [ -d $APPLE_PLUGIN_LIBRARY_ROOT ]; then
+    for folder in ""$APPLE_PLUGIN_LIBRARY_ROOT""/*; do
+        if [ -d ""$folder"" ]; then
+            for item in ""$folder""/*; do
+                if [[ $item = *'.dSYM' ]]; then
+                    continue
+                elif [[ $item = *'.framework' ]]; then
+                    filename=$(basename $item)
+                    echo ""    Embedding Apple plug-in framework $filename""
+                    echo ""      Source: $item""
+                    echo ""      Destination: $dstFrameworkFolder/$filename""
+                    if [ ! -z ""$EXPANDED_CODE_SIGN_IDENTITY"" ]; then
+                        echo ""      Code signing identity: $EXPANDED_CODE_SIGN_IDENTITY""
+                        codesign --force --sign $EXPANDED_CODE_SIGN_IDENTITY --timestamp\=none --generate-entitlement-der $item
+                    fi
+                    ditto $item ""$dstFrameworkFolder/$filename""
+                    break
+                elif [[ $item = *'.bundle' ]]; then
+                    filename=$(basename $item)
+                    echo ""    Embedding Apple plug-in bundle $filename""
+                    echo ""      Source: $item""
+                    echo ""      Destination: $dstBundleFolder/$filename""
+                    if [ ! -z ""$EXPANDED_CODE_SIGN_IDENTITY"" ]; then
+                        echo ""      Code signing identity: $EXPANDED_CODE_SIGN_IDENTITY""
+                        codesign --force --sign $EXPANDED_CODE_SIGN_IDENTITY --timestamp\=none --generate-entitlement-der $item
+                    fi
+                    ditto $item ""$dstBundleFolder/$filename""
+                    break
+                fi
+            done
+        fi
+    done
+    echo ""Completed search of libraries in the Apple native plug-in folder root.""
+else
+    echo ""No Apple plug-in library path found at $APPLE_PLUGIN_LIBRARY_ROOT""
+    echo ""Please build a Development Build in Unity for this script to log more information.""
+    exit 1
+fi
+";
+
+            List<string> inputPaths = new();
+            project.InsertShellScriptBuildPhase(7, unityFrameworkTargetGuid,
+                "Embed Apple Plug-in Libraries",
+                "/bin/sh",
+                embedApplePluginScript,
+                inputPaths);
 
             // Overwrite project
             project.WriteToFile(projectPath);
